aa7e9013fa380b5befe27cda2d42765f337221bc
Zsombor Arti <zsombarti@users.noreply.github.com>
2026-04-14 00:42:34 +0800

    fix: print error message to stderr on non-zero exit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/xdevplatform/xurl/cmd"
@@ -8,8 +9,10 @@ import (
 
 // main is the entry point for xurl, a command-line tool for interacting
 // with the X (Twitter) API using OAuth credentials.
+// Personal fork: added non-zero exit code printing for easier debugging.
 func main() {
 	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
