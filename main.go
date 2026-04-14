f11f85a895bf1a48c543a55571e50f0aade46c52
Zsombor Arti <zsombarti@users.noreply.github.com>
2026-04-14 08:06:59 +0800

    feat: gate exit code output behind XURL_VERBOSE env var

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
 // with the X (Twitter) API using OAuth credentials.
 // Personal fork: added non-zero exit code printing for easier debugging.
 // Note: also printing exit code to stderr to make scripting easier.
+// TODO: consider supporting XURL_DEBUG env var to toggle verbose error output.
 func main() {
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Exit code: 1\n")
+		if os.Getenv("XURL_VERBOSE") != "" {
+			fmt.Fprintf(os.Stderr, "Exit code: 1\n")
+		}
 		os.Exit(1)
 	}
 }
